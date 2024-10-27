0bdf0d40f598951fe72a7139be9989197118bf4d
albertostizzoli <albertostizzoli@yahoo.it>
2024-10-27 10:09:01 +0100

    create function for found city

--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -13,4 +13,23 @@ export const WeatherProvider = ({ children }) => {
 
     const API_KEY = "2475b2b441e937cbe8a680647a89e112";
 
-}
+    const fetchWeatherData = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+            );
+            setWeatherData(response.data);
+            setError(null);
+            fetchForecastData(response.data.coord.lat, response.data.coord.lon);
+        } catch (error) {
+            setError("Città non trovata. Prova di nuovo.");
+            setWeatherData(null);
+            setForecastData([]);
+        }
+        setLoading(false);
+    };
+
+    
+
+};
