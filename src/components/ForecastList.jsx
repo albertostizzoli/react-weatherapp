afa21ed6eecb80f08ec573ad126baf1137a297c2
albertostizzoli <albertostizzoli@yahoo.it>
2024-11-11 10:20:18 +0100

    remove actual day in forecastlist

--- src/components/ForecastList.jsx
+++ src/components/ForecastList.jsx
@@ -6,12 +6,14 @@ const ForecastList = () => {
     // Recupera forecastData dal contesto tramite il hook useWeather
     const { forecastData } = useWeather();
 
+    const today = new Date().toLocaleDateString("it-IT");
+
     return (
         <div className="mt-8 overflow-x-auto py-5">
             <table className="min-w-full bg-white rounded-lg shadow-md">
                 <thead>
                     <tr className="text-white bg-blue-600">
-                        <th className="px-4 py-2 text-center font-semibold">Data</th>
+                        <th className="px-4 py-2 text-center font-semibold">Giorno</th>
                         <th className="px-4 py-2 text-center font-semibold">Temperatura</th>
                         <th className="px-4 py-2 text-center font-semibold">Temp. Max</th>
                         <th className="px-4 py-2 text-center font-semibold">Temp. Min</th>
@@ -22,12 +24,15 @@ const ForecastList = () => {
                 </thead>
                 <tbody>
                     {forecastData
-                        .filter((_, index) => index % 8 === 0) // Filtra per ottenere una previsione giornaliera
+                        .filter((forecast, index) =>
+                            index % 8 === 0 &&
+                            new Date(forecast.dt * 1000).toLocaleDateString("it-IT") !== today
+                        ) // Filtra per ottenere una previsione giornaliera
                         .map((forecast, index) => (
                             <tr key={index} className="border-b">
                                 {/* Data della previsione */}
                                 <td className="px-4 py-2 text-gray-600">
-                                {new Date(forecast.dt * 1000).toLocaleDateString("it-IT", { weekday: 'long' })}
+                                    {new Date(forecast.dt * 1000).toLocaleDateString("it-IT", { weekday: 'long' })}
                                 </td>
 
                                 {/* Temperatura prevista */}
