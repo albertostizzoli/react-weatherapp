2fae76625898aee61bddb309b7dd4ce5e4808fe6
albertostizzoli <albertostizzoli@yahoo.it>
2024-11-12 16:20:05 +0100

    create function onDrag

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -5,14 +5,14 @@ import { useWeather } from "../context/WeatherContext";
 const HourlyForecast = () => {
     const { forecastData, loading, error } = useWeather(); // dati del contesto
     const forecastContainerRef = useRef(null); // Ref per il contenitore del carosello
-    const [ setIsDragging] = useState(false); // Stato per tracciare se il carosello è in drag
-    const [ setStartX] = useState(0); // Posizione iniziale del click per il drag
+    const [isDragging, setIsDragging] = useState(false); // Stato per tracciare se il carosello è in drag
+    const [startX, setStartX] = useState(0); // Posizione iniziale del click per il drag
     const [scrollLeft, setScrollLeft] = useState(0); // Posizione iniziale dello scroll per il drag
     const [velocity, setVelocity] = useState(0); // Velocità del trascinamento per lo scroll inerziale
     const animationFrameRef = useRef(null); // Ref per memorizzare l'animazione frame-by-frame
 
 
-    // Inizia il drag del carosello
+    // Drag del carosello
     const startDrag = (e) => {
         setIsDragging(true); //Imposta lo stato come "in drag"
         // Calcola la posizione iniziale rispetto al margine sinistro del carosello
@@ -23,4 +23,15 @@ const HourlyForecast = () => {
         cancelAnimationFrame(animationFrameRef.current); // Annulla qualsiasi animazione precedente
     };
 
+    // Funzione per gestire il drag quando l'utente muove il mouse
+    const onDrag = (e) => {
+        if (!isDragging) return; // Se non è in drag, interrompe l'esecuzione
+        const x = e.pageX - forecastContainerRef.current.offsetLeft; // Posizione corrente del mouse
+        const walk = (x - startX) * 1.5; // Distanza percorsa, con un fattore di velocità
+        forecastContainerRef.current.scrollLeft = scrollLeft - walk; // Sposta il carosello
+        setVelocity(walk); // Aggiorna la velocità corrente
+    };
+
 };
+
+export default HourlyForecast;
