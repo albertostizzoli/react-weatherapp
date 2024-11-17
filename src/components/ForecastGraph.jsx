61caf73a0b0ad3245ec3d41aa5572eace7ba97c2
albertostizzoli <albertostizzoli@yahoo.it>
2024-11-17 09:50:59 +0100

    create function for to estract dates of atmospheric pressure

--- src/components/ForecastGraph.jsx
+++ src/components/ForecastGraph.jsx
@@ -4,6 +4,12 @@ import { useWeather } from "../context/WeatherContext";
 
 const ForecastGraph = () => {
     const { forecastData } = useWeather();
+
+    // Estraggo i dati per la pressione atmosferica e le etichette temporali per il grafico
+    const pressureData = forecastData.map((data) => data.main.pressure);
+    const labels = forecastData.map((data) => 
+        new Date(data.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})
+    );
 };
 
 export default ForecastGraph;
